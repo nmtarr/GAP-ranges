f84072dba4750fda5506aed707bdc4f48fc31a80
Nathan Tarr <nmtarr@ncsu.edu>
2019-08-12 21:24:29 -0400

    Moved id_species_concepts.py to repo_functions.py for tidiness.

--- repo_functions.py
+++ repo_functions.py
@@ -219,3 +219,21 @@ def make_evaluation_db(gap_id, outDir, shucLoc, inDir):
     """
     cursor.executescript(sql2)
 
+def get_GBIF_species_key(scientific_name):
+    """
+    Description: Species-concepts change over time, sometimes with a spatial
+    component (e.g., changes in range delination of closely related species or
+    subspecies).  Retrieval of data for the wrong species-concept would introduce
+    error.  Therefore, the first step is to sort out species concepts of different
+    datasets to identify concepts that can be investigated.
+
+    For this project/effort, individual species-concepts will be identified,
+    crosswalked to concepts from various datasets, and stored in a table within
+    a database.
+
+    For now, a single species has been manually entered into species-concepts
+    for development.
+    """
+    from pygbif import species
+    key = species.name_backbone(name = 'Lithobates capito', rank='species')['usageKey']
+    return key
